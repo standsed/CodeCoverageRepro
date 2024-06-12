0f7cbb22a871a14bcc3dcd98319d495897dbbbf7
standsed <standsed@gmail.com>
2024-06-12 11:03:09 +0300

    init

---
+++ Repro.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Repro.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Class1.Method1();
+    }
+}
---
+++ Repro/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Repro;
+
+public static class Class1
+{
+    public static async Task<string> Method1() { return "asdf"; }
+
+    public static async Task<string> Method2<T>(T val) { if (val is string x) { return x; } throw new NotSupportedException(); }
+
+    public static async Task<string> Method3<T, TU>(T val) { return "asdf"; }
+}
